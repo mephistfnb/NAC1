cd6bce972e71e8f72f88bfd57f962c74f67b7cc7
mephistfnb <fv.aegis@gmail.com>
2019-05-26 17:34:28 -0300

    Initial commit

---
+++ app/src/main/java/com/example/snappiano/SnapLayout1.java
@@ -0,0 +1,15 @@
+package com.example.snappiano;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class SnapLayout1 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.snap_layout1);
+    }
+}
+
+
